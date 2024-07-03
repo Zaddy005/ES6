727c7df42f7b8dff152e2eb7fe8ef3cdcdf86938
zaddy <aungnaingphyo005@gmail.com>
2024-07-04 01:04:57 +0630

    add new file

---
+++ toolwebpack/src/index.js
@@ -0,0 +1,11 @@
+import {Phone} from "./modules/phone.js";
+
+const obj1 = new Phone("iphone",'5g');
+console.log(obj1);
+// console.log(obj1.brand);  // iphone
+
+console.log((obj1.getbrandname()));
+console.log((obj1.getphonenetwork()));
+console.log((obj1.getcolorcode()));
+
+
